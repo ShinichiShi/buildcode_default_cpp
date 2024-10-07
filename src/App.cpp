63859d38bdac6e62215554aaa73e501b26706266
ShinichiShi <supreeth2020@gmail.com>
2024-10-07 17:15:09 +0530

    katr le bhai

--- src/App.cpp
+++ src/App.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
-#include <string>
+#include <cstring>  
+using namespace std;
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <tar file>" << std::endl;
+        return 1;
+    }
 
-int main(int argc, char** argv)
-{
-	// Put your code here!
+    std::ifstream tarFile(argv[1], std::ios::binary);
+    if (!tarFile) {
+        cerr << "Error: Could not open the file " << argv[1] << std::endl;
+        return 1;
+    }
 
-	return 0;
+    char filename[101]; 
+    memset(filename, 0, sizeof(filename)); 
+
+    tarFile.read(filename, 100); 
+    if (!tarFile) {
+        cerr << "Error: Could not read the tar header" << std::endl;
+        return 1;
+    }
+
+    cout << filename << endl;
+
+    tarFile.close();
+
+    return 0;
 }
